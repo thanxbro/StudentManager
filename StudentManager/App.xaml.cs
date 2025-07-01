1777135d5357b35662f46bc9e6f328c290f42bd8
Roman Aminov <romanas911@icloud.com>
2025-07-01 11:33:20 +0800

    Внедрили DI в проект и сделали запуск приложения через него

--- StudentManager/App.xaml.cs
+++ StudentManager/App.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StudentManager.View;
 using System.Windows;
 
 namespace StudentManager
@@ -9,6 +10,30 @@ namespace StudentManager
     /// </summary>
     public partial class App : Application
     {
+        private IHost _host;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //Добавляем сервисы в контейнер
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+            builder.Services.AddSingleton<MainWindow>();
+
+            _host = builder.Build();
+
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+
+            _host.StartAsync();
+
+
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            _host.Dispose();
+        }
     }
 
 }
--- StudentManager/View/MainWindow.xaml.cs
+++ StudentManager/View/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace StudentManager
+namespace StudentManager.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
