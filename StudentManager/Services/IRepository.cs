6d16dddbff79d484834545ea87c6fae7959ba62c
Roman Aminov <romanas911@icloud.com>
2025-07-01 13:22:56 +0800

    реализован сервис по управлению записями студетов в базе данных

---
+++ StudentManager/Services/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using StudentManager.DataBase.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager.Services
+{
+    public interface IRepository
+    {
+        Task AddStudent(Student student);
+        Task RemoveStudent(Student student);
+        Task<List<Student>> GetAllStudents();
+
+        Task UpdateStudent(Student student);
+    }
+}
---
+++ StudentManager/Services/StudentRepositoryService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManager.DataBase;
+using StudentManager.DataBase.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager.Services
+{
+    public class StudentRepositoryService : IRepository
+    {
+        private DataBaseContext _db;
+        public StudentRepositoryService(DataBaseContext db) 
+        {
+            _db = db;
+        }
+        public async Task AddStudent(Student student)
+        {
+            await _db.AddAsync(student);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<List<Student>> GetAllStudents()
+        {
+            return await _db.Set<Student>().ToListAsync();
+        }
+
+        public async Task RemoveStudent(Student student)
+        {
+            _db.Remove(student);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task UpdateStudent(Student student)
+        {
+            _db.Update(student);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
